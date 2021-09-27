ea2f9b0ea2627368176ed04ecc938d9e6ec60557
Monica Powell <monica@aboutmonica.com>
2021-09-27 10:14:43 -0400

    WIP solution for exercise 5

---
+++ site/src/templates/planet.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default function planetPage({ data }) {
+  const planet = data.mdx.frontmatter
+  const { name, orbit, img, maxTemp } = planet
+  return (
+    <Layout>
+      <div>
+        <h1>{name}</h1>
+        <h2>{orbit}</h2>
+        {maxTemp}
+        <img src={img} alt={name} width="250px" />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ($slug: String!) {
+    mdx(slug: { eq: $slug }) {
+      frontmatter {
+        name
+        orbit
+        img
+        maxTemp
+      }
+    }
+  }
+`
+
+// query fetchPlanets {
+//   allMdx {
+//     edges {
+//       node {
+//         id
+//         frontmatter {
+//           img
+//           name
+//           maxTemp
+//           orbit
+//         }
+//       }
+//     }
+//   }
+// }
