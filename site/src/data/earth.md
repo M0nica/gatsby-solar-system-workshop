daacde0e10f13e2a9375a968168a70a5ee60a99d
Monica Powell <monica@aboutmonica.com>
2021-10-04 15:16:29 -0400

    example of adding mdx to planet page

--- site/src/data/earth.md
+++ site/src/data/earth.md
@@ -6,3 +6,16 @@ maxTemp: "134.1 °F (56.7 °C)"
 ---
 
 Hello fellow Earthlings! This is the current text associated with planet Earth.
+
+## Facts
+
+<Callout variant="comet"> If you look up at the sky this year you might see Comet Leonard! </Callout>
+
+<Callout variant="star"> A shooting star is when debris hits Earth</Callout>
+
+<Callout variant="moon"> The Lunar Month is 28 days long. </Callout>
+
+<Callout variant="sun"> The Sun accounts for 99.86% of the mass in the solar system.</Callout>
+
+## Soundtrack 
+<Spotify spotifyLink="album/7253n8oKnyOumxC2trgmM8" />
--- site/src/templates/planet.js
+++ site/src/templates/planet.js
@@ -1,21 +1,32 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXEmbedProvider } from "mdx-embed"
+import Callout from "../components/callout"
+
+const components = {
+  Callout,
+}
 
 export default function planetPage({ data }) {
   const planet = data.mdx.frontmatter
   const { name, orbit, img, maxTemp } = planet
   return (
-    <Layout>
-      <div>
-        <h1>{name}</h1>
-        <h2>{orbit}</h2>
-        {maxTemp}
-        <img src={img} alt={name} width="250px" />
-        <MDXRenderer title={name}>{data.mdx.body}</MDXRenderer>
-      </div>
-    </Layout>
+    <MDXProvider components={components}>
+      <MDXEmbedProvider>
+        <Layout>
+          <div>
+            <h1>{name}</h1>
+            <h2>{orbit}</h2>
+            {maxTemp}
+            <img src={img} alt={name} width="250px" />
+            <MDXRenderer title={name}>{data.mdx.body}</MDXRenderer>
+          </div>
+        </Layout>
+      </MDXEmbedProvider>
+    </MDXProvider>
   )
 }
 
